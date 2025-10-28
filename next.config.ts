fd3fa9d36292db2027d7c62762ae204132fad105
Eduard <ed.kechedzhiev@gmail.com>
2025-10-28 14:42:44 -0400

    ci/cd

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'out',
+  basePath: '',
   reactStrictMode: true,
   images: {
     unoptimized: true,
